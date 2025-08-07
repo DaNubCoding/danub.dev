a4db46269170e01531dfb5979c2d9bc74ec38cfc
DaNubCoding <danubcoding@gmail.com>
2025-08-07 05:00:57 -0400

    test text and basic scss formatting

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
 
+	import "../lib/styles/global.scss";
+
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>danub.dev</title>
+
+  <link
+    href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;500;600;700&display=swap"
+    rel="stylesheet"
+  />
 </svelte:head>
 
-{@render children?.()}
+<main>
+	{@render children?.()}
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>danub.dev 💜</h1>
-<h3>*site under construction*</h3>
+<h1>DaNub</h1>
+
+<p>Hey there random netizen! Welcome to my personal site :D</p>
+<p>
+  My name is Andrew Wang, I go by DaNub online, or any version of that followed
+  by a suffix (e.g. DaNubCoding).
+</p>
+{#each { length: 5 }}
+  <p>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam aliquid,
+    veritatis et, placeat laborum amet eius inventore voluptatem sapiente libero
+    mollitia minus distinctio animi dolor aliquam similique reprehenderit,
+    magnam possimus!
+  </p>
+{/each}
+
+<style lang="scss">
+  p {
+    margin: 1em 5em;
+    line-height: 1.5em;
+  }
+</style>
